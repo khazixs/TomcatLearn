cf04dbb941da196ea43e80ae4249993c83602916
Dandelion <heiyubaidejianying@gamil.com>
2019-11-05 17:40:07 +0800

    201911051740

--- MybatisLearn/src/main/java/com/IceCoLa/dao/IUserDao.java
+++ MybatisLearn/src/main/java/com/IceCoLa/dao/IUserDao.java
@@ -1,6 +1,7 @@
 package com.IceCoLa.dao;
 
 import com.IceCoLa.domain.User;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -8,5 +9,6 @@ import java.util.List;
 * 用户持久层接口*/
 public interface IUserDao {
 //    查询所有对象
+    @Select("select * from user")
     List<User> findAll();
  }
